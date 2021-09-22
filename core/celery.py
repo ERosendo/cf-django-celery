c71fe1c694459fc42f978ce57b759e81fe4a46b1
Eduardo Rosendo <eduardojra96@gmail.com>
2021-09-22 04:28:29 -0400

    celery credentiasl

--- core/celery.py
+++ core/celery.py
@@ -8,10 +8,10 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 app = Celery('engine')
 
-""" app.conf.broker_use_ssl = {
+app.conf.broker_use_ssl = {
   'cert_reqs': ssl.CERT_NONE
 }
- """
+
  
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
