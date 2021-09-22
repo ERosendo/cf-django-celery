c7b92987d963238492e0c97ba9c5b40ce482cd02
Eduardo Rosendo <eduardojra96@gmail.com>
2021-09-22 18:39:00 -0400

    change .sh files

--- init_app.sh
+++ init_app.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # From http://blog.banck.net/2014/12/deploying-a-django-application-to-cloud-foundry/#comment-3967
 # echo "------ Create database tables ------"
-cd core/
+cd core2/
 python manage.py migrate
 
 # run app
--- init_celery.sh
+++ init_celery.sh
@@ -1,3 +1,3 @@
 sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
-cd core/
+cd core2/
 celery -A core worker -l debug -c 3
